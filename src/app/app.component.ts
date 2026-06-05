fb92c87e42a422d52b31130adb19141e50d8a3f7
Denyson Cardoso <denyson.cardoso@hotmail.com>
2026-06-05 19:37:59 -0300

    feat: add template-driven forms with sync validation component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
 import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterModule, RouterOutlet, Routes} from '@angular/router';
+
+interface Menuitem {
+   title: string;
+   submenu: {
+      title: string;
+      url: string;
+   }[]
+}
+
+
+function createMenuTitle(route: string) {
+   return route.split('-').map(word => word.charAt(0).toUpperCase() + word.slice(1))
+       .join(' ');
+
+}
+
+function createMenuItems(routes: Routes): Menuitem[] {
+   return routes.map((route) => {
+      const title = createMenuTitle(route.path!);
+
+      const submenus = route.children!.map(child => {
+         const childTitle = createMenuTitle(child.path!);
+         const childUrl = `${route.path}/${child.path!}`;
+
+         return {
+            title: childTitle,
+            url: childUrl
+         };
+      });
+
+      return {
+         title,
+         submenus,
+      };
+   });
+
+}
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush
+   selector: 'app-root',
+   imports: [RouterModule],
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.css',
+   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class App {
-  protected readonly title = signal('formularios-angular');
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {routes as templateDrivenFormsRoutes} from './features/template-driven-forms/routes'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'template-driven-forms',
+    children: templateDrivenFormsRoutes
+  },
+];
